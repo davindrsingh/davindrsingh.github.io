b4912b28745a8873f297f0f7de18f61cd2d27236
davinderiitgn <davinder.singh@iitgn.ac.in>
2020-04-28 04:32:45 +0530

    index page

---
+++ js/navjs.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+		$("#home").click(function(){
+			$(".navbar-nav .active").removeClass("active");
+
+			$("#home").addClass("active");
+		});
+
+		$("#projects").click(function(){
+			$(".navbar-nav .active").removeClass("active");
+			$("#projects").addClass("active");
+		});
+		$("#publ").click(function(){
+			$(".navbar-nav .active").removeClass("active");
+			$("#publ").addClass("active");
+		});
+		$("#resume").click(function(){
+			$(".navbar-nav .active").removeClass("active");
+			$("#resume").addClass("active");
+		});
+</script>
